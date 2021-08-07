c9c6b730afa00b5d735b33af4d02acf2e8b54611
torki <a.torki@hasintech.com>
2021-08-07 13:14:54 +0430

    docs(storybook): add week day name story demo

---
+++ src/stories/week-day-name.stories.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { createBaseStory, BaseTemplate, baseArgs } from './base';
+import {
+  createDateInstance,
+  format,
+  Picker,
+  WheelPickerSelectEvent,
+} from '../index'; // in your code: @persian-tools/persian-mobile-datepicker
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+// Types
+import type { ComponentStory } from '@storybook/react';
+import type { Event } from '../components/WheelPicker/index.types';
+
+const title = 'Week day name';
+export default createBaseStory(title);
+
+const stories = storiesOf(title, module);
+
+const BasePickerTemplate: ComponentStory<typeof Picker> = (args) => {
+  const [selectedDateValue, setSelectedDateValue] = React.useState<string>();
+  const [selectedDateEvents, setSelectedDateEvents] = React.useState<
+    Array<Event>
+  >([]);
+
+  function handleEvent(eventType: string) {
+    return (data: WheelPickerSelectEvent) => {
+      setSelectedDateValue(format(data.date!, 'd MMMM yyyy'));
+      setSelectedDateEvents(data.events);
+      action(eventType)(data);
+    };
+  }
+
+  return (
+    <BaseTemplate value={selectedDateValue!} events={selectedDateEvents!}>
+      <Picker
+        {...args}
+        onChange={handleEvent('onChange')}
+        onSubmit={handleEvent('onSubmit')}
+      />
+    </BaseTemplate>
+  );
+};
+
+stories.add(title, (args: any) => <BasePickerTemplate {...args} />, {
+  component: Picker,
+  args: {
+    isOpen: true,
+    theme: 'light',
+    config: {
+      year: {},
+      month: {},
+      day: {},
+    },
+    addDayName: true,
+    initialValue: createDateInstance({ year: 1400, month: 1, day: 6 }),
+  },
+  argTypes: baseArgs,
+});

0c2c5dc3d194405966069947a1247638262ea4a7
torki <a.torki@hasintech.com>
2021-08-07 13:17:09 +0430

    docs(storybook): added all props doc in args table

--- src/stories/base.tsx
+++ src/stories/base.tsx
@@ -42,21 +42,131 @@ export function createBaseStory(title: string): ComponentMeta<typeof Picker> {
   return {
     title,
     component: Picker,
-    argTypes: {
-      theme: {
-        control: {
-          type: 'inline-radio',
-          options: ['light', 'dark'],
-        },
-      },
-    },
-    parameters: {
-      docs: {
-        source: {
-          type: 'code',
-        },
-      },
-    },
+    argTypes: baseArgs,
     decorators: [(Story): JSX.Element => <Story />],
   };
 }
+
+export const baseArgs = {
+  theme: {
+    control: {
+      type: 'inline-radio',
+      options: ['light', 'dark', 'auto'],
+      defaultValue: 'light',
+    },
+  },
+  config: {
+    control: {
+      type: 'object',
+    },
+  },
+  classNamePrefix: {
+    name: 'classNamePrefix',
+    description: 'Elements className may have a prefix',
+    control: {
+      type: 'text',
+      defaultValue: 'persian-datepicker',
+    },
+  },
+  title: {
+    name: 'title',
+    description: 'WheelPicker title',
+    control: {
+      type: 'text',
+      defaultValue: '',
+    },
+  },
+  minDate: {
+    name: 'minDate',
+    description: 'Specifies the minimum selectable day by user',
+    control: {
+      type: 'date',
+    },
+  },
+  maxDate: {
+    name: 'maxDate',
+    description: 'Specifies the maximum selectable day by user',
+    control: {
+      type: 'date',
+    },
+  },
+  endYear: {
+    name: 'endYear',
+    description: 'The Minimum selectable year',
+    control: {
+      type: 'number',
+    },
+  },
+  startYear: {
+    name: 'startYear',
+    description: 'The Maximum selectable year',
+    control: {
+      type: 'number',
+    },
+  },
+  addDayName: {
+    name: 'addDayName',
+    description: 'Add the name of the day of the week',
+    control: {
+      type: 'boolean',
+      defaultValue: false,
+    },
+  },
+  isOpen: {
+    name: 'isOpen',
+    description: 'Picker open status',
+    control: {
+      type: 'boolean',
+      defaultValue: false,
+    },
+  },
+  submitText: {
+    name: 'submitText',
+    description: 'Submit button text',
+    control: {
+      type: 'text',
+      defaultValue: 'تایید',
+    },
+  },
+  cancelText: {
+    name: 'cancelText',
+    description: 'Cancel button text',
+    control: {
+      type: 'text',
+      defaultValue: 'انصراف',
+    },
+  },
+  showCancelButton: {
+    name: 'showCancelButton',
+    description: 'Display Cancel button',
+    control: {
+      type: 'boolean',
+      defaultValue: true,
+    },
+  },
+  disableSheetDrag: {
+    name: 'disableSheetDrag',
+    description: 'Disable drag for the sheet content',
+    control: {
+      type: 'boolean',
+      defaultValue: true,
+    },
+  },
+  disableSheetHeaderDrag: {
+    name: 'disableSheetHeaderDrag',
+    description: 'Disable drag for the sheet header',
+    control: {
+      type: 'boolean',
+      defaultValue: false,
+    },
+  },
+  height: {
+    name: 'height',
+    description: 'Height of Picker Sheet modal',
+    control: {
+      type: 'number',
+      defaultValue: 385,
+      min: 385,
+    },
+  },
+};

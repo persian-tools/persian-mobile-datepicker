03ee37c395db336aefca9662eb1ed7d9e5dcca91
torki <a.torki@hasintech.com>
2021-08-08 04:12:22 +0430

    docs(storybook): add storybook console addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,32 @@
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { withConsole } from '@storybook/addon-console';
+import { addDecorator } from '@storybook/react';
+
+addDecorator((storyFn, context) =>
+  withConsole({
+    panelExclude: [
+      /Warning/,
+      /error/,
+      /warn/,
+      /Error/,
+      /Illegal mix/,
+      /Ignored an update to unaccepted module/,
+      /but no exported stories/,
+      /a story file/,
+    ],
+    // consoleExclude: [
+    //   /error/,
+    //   /warn/,
+    //   /Warning/,
+    //   /Error/,
+    //   /Ignored an update to unaccepted module/,
+    //   /`waring message`/,
+    //   /Illegal mix/,
+    //   /`Found a story file for`/,
+    //   /but no exported stories/,
+    // ],
+  })(storyFn)(context),
+);
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },

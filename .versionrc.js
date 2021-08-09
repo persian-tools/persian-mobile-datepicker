da80d3330a37c6d43bf30005164ffe7555488e8d
torki <a.torki@hasintech.com>
2021-08-09 09:35:25 +0430

    chore: add versionrc file

---
+++ .versionrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  types: [
+    { type: 'chore', section: 'Minor Improvements', hidden: false },
+    { type: 'revert', section: 'Reverts', hidden: false },
+    { type: 'feat', section: 'Features', hidden: false },
+    { type: 'fix', section: 'Bug Fixes', hidden: false },
+    { type: 'improvement', section: 'Feature Improvements', hidden: false },
+    { type: 'docs', section: 'Docs', hidden: false },
+    { type: 'style', section: 'Styling', hidden: false },
+    { type: 'refactor', section: 'Code Refactoring', hidden: false },
+    { type: 'perf', section: 'Performance Improvements', hidden: false },
+    { type: 'test', section: 'Tests', hidden: true },
+    { type: 'build', section: 'Build System', hidden: true },
+    { type: 'ci', section: 'CI', hidden: true },
+    { type: 'release', section: 'Release', hidden: true },
+  ],
+};
--- release.config.js
+++ release.config.js
@@ -15,7 +15,7 @@ module.exports = {
       {
         assets: ['package.json', 'CHANGELOG.md'],
         message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}', // eslint-disable-line
+          'release: v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}', // eslint-disable-line
       },
     ],
   ],
